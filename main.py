9384b6eae3bcb9ccefd0676be085326cea460e70
24223 <24223@students.mrgs.school.nz>
2026-05-13 11:05:38 +1200

    adjusting image

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ window.title("RepFit")
 
 window.config(background="#99c5ff")
 
-photo=PhotoImage(file='C:\\Users\\24223\\Downloads\\Dumbell.png')
+photo = PhotoImage(file='Images/Dumbell.png')
 
-label = Label(window,text="RepFit",font=('Arial',200,'bold'),fg='black',bg='#99c5ff')
-label.pack()
-label = Label(window,text="Progress, Not Perfection",font=('Arial', 50,'normal'),fg='black', bg='#99c5ff')
-label.place(x=0,y=80)
-label.pack()
-label = Label(window, image=photo, bg='#99c5ff')
-label.place(x=100,y=100)
-label.pack()
+label_title = Label(window,
+                    text="RepFit",
+                    font=('Arial',200,'bold'),
+                    fg='black'
+                    ,bg='#99c5ff',
+                    compound='left')
+
+label_title.pack()
+label_sub = Label(window,text="Progress, Not Perfection",font=('Arial', 50,'normal'),fg='black', bg='#99c5ff')
+label_sub.place(x=0,y=80)
+label_sub.pack()
+label = Label(window, image=photo, bg='#99c5ff',)
+label.place(x=0,y=0)
+label.pack(pady=(5,20))
+label.pack(padx=(10,10))
 window.mainloop()
 
