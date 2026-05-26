612dcab64c536826e017c1833f3bc914c8e09171
24223 <24223@students.mrgs.school.nz>
2026-05-27 11:10:11 +1200

    Altering image code

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 from tkinter import *
 
+import ImageTk
+
 window = Tk()
 window.geometry("420x420")
 window.title("RepFit")
 
-window.config(background="#99c5ff")
+import tkinter as tk
+from PIL import Image
 
-photo = PhotoImage(file='Images/Dumbell.png')
+root = tk.Tk()
+
+image_filename = "Dumbell.png"
+
+load = Image.open('Images/Dumbell.png')
+
+img = Image.open('Images/Dumbell.png')
+render = ImageTk.PhotoImage(load)
+
+img_label = tk.Label(root, Image=render)
+img_label.pack()
+
+root.mainloop()
+
+window.config(background="#99c5ff")
 
 label_title = Label(window,
                     text="RepFit",
                     font=('Arial',200,'bold'),
-                    fg='black'
-                    ,bg='#99c5ff',
+                    fg='black',
+                    bg='#99c5ff',
                     compound='left')
 
 label_title.pack()
 label_sub = Label(window,text="Progress, Not Perfection",font=('Arial', 50,'normal'),fg='black', bg='#99c5ff')
 label_sub.place(x=0,y=80)
 label_sub.pack()
-label = Label(window, image=photo, bg='#99c5ff',)
-label.place(x=0,y=0)
-label.pack(pady=(5,20))
-label.pack(padx=(10,10))
-window.mainloop()
-
