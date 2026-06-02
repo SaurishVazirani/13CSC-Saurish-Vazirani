81ff8f216cdb83c681da67d18ebe0d0a0d34bc65
24223 <24223@students.mrgs.school.nz>
2026-06-03 11:10:07 +1200

    restarting and creating list for colours

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
-from tkinter import *
-
-import ImageTk
-
-window = Tk()
-window.geometry("420x420")
-window.title("RepFit")
-
 import tkinter as tk
-from PIL import Image
+from tkinter import font as tkfont
+import math
 
-root = tk.Tk()
+#list for colours
 
-image_filename = "Dumbell.png"
+BG_LIGHT = "#99c5ff"
+BG_NAV = "#3770df"
+BTN_BG = "#000000"
+BTN_FG = "ffffff"
+TEXT_DARK = "000000"
+TEXT_NAV = "ffffff"
 
-load = Image.open('Images/Dumbell.png')
 
-img = Image.open('Images/Dumbell.png')
-render = ImageTk.PhotoImage(load)
-
-img_label = tk.Label(root, Image=render)
-img_label.pack()
-
-root.mainloop()
-
-window.config(background="#99c5ff")
-
-label_title = Label(window,
-                    text="RepFit",
-                    font=('Arial',200,'bold'),
-                    fg='black',
-                    bg='#99c5ff',
-                    compound='left')
-
-label_title.pack()
-label_sub = Label(window,text="Progress, Not Perfection",font=('Arial', 50,'normal'),fg='black', bg='#99c5ff')
-label_sub.place(x=0,y=80)
-label_sub.pack()
