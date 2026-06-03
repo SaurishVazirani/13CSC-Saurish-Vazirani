847b44f8193f109c8817b9a36d08ae2041ece19d
24223 <24223@students.mrgs.school.nz>
2026-06-04 09:55:04 +1200

    creating app class

--- main.py
+++ main.py
@@ -11,4 +11,16 @@ BTN_FG = "ffffff"
 TEXT_DARK = "000000"
 TEXT_NAV = "ffffff"
 
+W, H = 480, 320
 
+class App(tk.TK):
+    def __init__(self):
+        super().__init__()
+        self.title("RepFit")
+        self.resizable(False, False)
+        self.geometry(f"{W}x{H}")
+        self.configure(bg=BG_LIGHT)
+
+        self.fonts = {
+            "title": tkfont.Font(family="Impact", size=62, weight="Bold")
+        }
