058124dda59c275eb00ad8a305b8b301c2ffc708
24223 <24223@students.mrgs.school.nz>
2026-06-15 13:29:03 +1200

    creating window layout and loading images in

--- main.py
+++ main.py
@@ -37,13 +37,6 @@ TEXT_NAV = "ffffff"
 W, H = 1400, 780
 Nav_H = 110
 
-window = tk.Tk()
-window.geometry(f"{W}x{H}")
-window.title("RepFit")
-
-window.configure(background=BG_LIGHT)
-
-
 def load(name: str, size: tuple) -> ImageTk.PhotoImage:
     path = os.path.join(BASE, ASSETS[name])
     img  = Image.open(path).convert("RGBA").resize(size, Image.LANCZOS)
@@ -58,7 +51,23 @@ def nav_btn(parent, text, icon_normal, icon_hover, cmd):
                         font=("Arial", 13, "bold"))
     lbl_text.pack()
 
-class App(tk.Tk):
+    def on_enter(_):
+        frm.configure(bg="#4a6ae8")
+        lbl_icon.configure(bg="#4a6ae8")
+        lbl_text.configure(bg="#4a6ae8")
+
+    def on_leave(_):
+        frm.configure(bg=BG_NAV)
+        lbl_icon.configure(bg=BG_NAV)
+        lbl_text.configure(bg=BG_NAV)
+
+    for w in (frm, lbl_icon, lbl_text):
+        w.bind("<Enter>", on_enter)
+        w.bind("<Leave>", on_leave)
+        w.bind("<Button-1>", lambda e:cmd())
+    return frm
+
+class RepFitApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("RepFit")
@@ -66,8 +75,47 @@ class App(tk.Tk):
         self.geometry(f"{W}x{H}")
         self.configure(bg=BG_LIGHT)
 
-        self.fonts = {
-            "title": tkfont.Font(family="Impact", size=62, weight="Bold")
-        }
+        #Storing workout logs
+        self.workout_log: list[dict] = []
 
-window.mainloop()
+        self._load_images()
+        self._build_ui()
+        self.show_home()
+
+    #loading images
+    def _load_images(self):
+        self.img = {}
+
+        #3d dumbells
+        self.img["db1"] = load("dumbell_3d_L.png", (230, 230))
+        self.img["db2"] = load("dumbell_3d_R.png", (230, 230))
+
+        #navbar images
+        NS = (60, 60)
+        self.img["home_b"] = load("Nav_home_b.png", NS)
+        self.img["home_w"] = load("Nav_home_w.png", NS)
+        self.img["log_b"] = load("Nav_log_b.png", NS)
+        self.img["log_w"] = load("Nav_log_w.png", NS)
+        self.img["workout_w"] = load("Nav_workout_w.png", NS)
+        self.img["workout_b"] = load("Nav_workout_b.png", NS)
+
+        #other images
+        self.img["stopwatch"] = load("Stopwatch.png", (90,90))
+        self.img["scale"] = load("Scale.png", (90,90))
+
+    #Window layout
+    def _build_ui(self):
+        self.content = tk.Frame(self, bg=BG_LIGHT, width=W, height=H - Nav_H)
+        self.content.pack(side="top", fill="both", expand=True)
+        nav = tk.Frame(self, bg=BG_NAV, height=Nav_H)
+        nav.pack(side="bottom", fill="x")
+        nav.pack_propagate(False)
+
+
+
+
+
+
+
+
+
